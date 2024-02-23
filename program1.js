0a2e66700f0ede1706243931a7d271060a47f569
Aadarsh Lokhande <lokhandeaadarsh28@gmail.com>
2024-02-23 13:15:13 +0530

    logic commit

--- program1.js
+++ program1.js
@@ -3,9 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+        const stack = [];
     
+        const openBrackets = ['(', '[', '{'];
+        const closeBrackets = [')', ']', '}'];
+    
+        for (let i = 0; i < s.length; i++) {
+            const char = s[i];
+    
+            if (openBrackets.includes(char)) {
+                stack.push(char);
+            } else if (closeBrackets.includes(char)) {
+                if (stack.length === 0) {
+                    return false;
+                }
+                const lastOpenBracket = stack[stack.length - 1];
+                const index = closeBrackets.indexOf(char);
+                if (lastOpenBracket !== openBrackets[index]) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+    
+        return stack.length === 0;
+   
 };
 
 module.exports = { isValid };
-
-
